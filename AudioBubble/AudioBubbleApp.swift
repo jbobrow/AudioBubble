2565034a66f0c84e77ee24be87bc8adb93a2c44d
Jonathan Bobrow <jbobrow@gmail.com>
2025-05-08 10:25:53 -0400

    Initial Commit

---
+++ AudioBubble/AudioBubbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioBubbleApp.swift
+//  AudioBubble
+//
+//  Created by Jonathan Bobrow on 5/8/25.
+//
+
+import SwiftUI
+
+@main
+struct AudioBubbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
